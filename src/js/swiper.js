2397d93809fceb7d13410ba51b75f6b60e787be1
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2025-04-25 20:12:56 +0300

    reviews

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,19 +1,33 @@
+// const swiper = new Swiper('.swiper', {
+//   speed: 400,
+
+//   // Default parameters
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   // Responsive breakpoints
+
+//   // when window width is >= 835px
+//   835: {
+//     slidesPerView: 2,
+//     spaceBetween: 16,
+//   },
+//   // when window width is >= 1440px
+//   1440: {
+//     slidesPerView: auto,
+//     spaceBetween: 40,
+//   },
+// });
+
+import Swiper from 'swiper';
+// import 'swiper/css';
+
 const swiper = new Swiper('.swiper', {
-  speed: 400,
-
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-
-  // when window width is >= 835px
-  835: {
-    slidesPerView: 2,
-    spaceBetween: 16,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
   },
-  // when window width is >= 1440px
-  1440: {
-    slidesPerView: auto,
-    spaceBetween: 40,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
