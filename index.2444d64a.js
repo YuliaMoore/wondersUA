e28fe5bb98f02f0932f43201eb9ac164301118c5
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2025-04-25 17:13:20 +0000

    Deploying to gh-pages from @ YuliaMoore/wondersUA@2397d93809fceb7d13410ba51b75f6b60e787be1 🚀

--- index.2444d64a.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{speed:400,slidesPerView:1,spaceBetween:10,835:{slidesPerView:2,spaceBetween:16},1440:{slidesPerView:auto,spaceBetween:40}});
-//# sourceMappingURL=index.2444d64a.js.map
